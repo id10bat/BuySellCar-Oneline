d6ac4c3dd5e1453bf4ba27fa87d495b3ed90b08e
npsk26 <my_nutthapat-26_@hotmail.com>
2018-11-15 06:15:44 +0700

    img to base64

--- src/components/pages.js
+++ src/components/pages.js
@@ -13,39 +13,40 @@ import {
 
 
 export const Pages = () => (
-                <Switch>
-                    <Route exact path="/BuySellCar-Online/" component={Home} />
+    <Switch>
+        <Route exact path="/" component={Home} />
+        <Route exact path="/BuySellCar-Online/" component={Home} />
 
-                    <Route path="/BuySellCar-Online/Authorities" component={Authorities} />
+        <Route path="/BuySellCar-Online/Authorities" component={Authorities} />
 
-                    <Route path="/BuySellCar-Online/ProblemDetermination" component={ProblemDetermination} />
-                    <Route path="/BuySellCar-Online/SystemRequest" component={SystemRequest} />
-                    <Route path="/BuySellCar-Online/Fishbone" component={Fishbone} />
-                    <Route path="/BuySellCar-Online/SystemScopeDocument" component={SystemScopeDocument} />
-                    <Route path="/BuySellCar-Online/ProjectSchedule" component={ProjectSchedule} />
-                    <Route path="/BuySellCar-Online/FeasibilityStudy" component={FeasibilityStudy} />
+        <Route path="/BuySellCar-Online/ProblemDetermination" component={ProblemDetermination} />
+        <Route path="/BuySellCar-Online/SystemRequest" component={SystemRequest} />
+        <Route path="/BuySellCar-Online/Fishbone" component={Fishbone} />
+        <Route path="/BuySellCar-Online/SystemScopeDocument" component={SystemScopeDocument} />
+        <Route path="/BuySellCar-Online/ProjectSchedule" component={ProjectSchedule} />
+        <Route path="/BuySellCar-Online/FeasibilityStudy" component={FeasibilityStudy} />
 
-                    <Route path="/BuySellCar-Online/DemandAnalysis" component={DemandAnalysis} />
-                    <Route path="/BuySellCar-Online/SystemRequirementSpecification" component={SystemRequirementSpecification} />
-                    <Route path="/BuySellCar-Online/Fact-FindingTechnics" component={Fact_FindingTechnics} />
+        <Route path="/BuySellCar-Online/DemandAnalysis" component={DemandAnalysis} />
+        <Route path="/BuySellCar-Online/SystemRequirementSpecification" component={SystemRequirementSpecification} />
+        <Route path="/BuySellCar-Online/Fact-FindingTechnics" component={Fact_FindingTechnics} />
 
-                    <Route path="/BuySellCar-Online/ProcessModeling" component={ProcessModeling} />
-                    <Route path="/BuySellCar-Online/ContextDiagram" component={ContextDiagram} />
-                    <Route path="/BuySellCar-Online/DFDDiagram" component={DFDDiagram} />
-                    <Route path="/BuySellCar-Online/ProcessDescription" component={ProcessDescription} />
-                    <Route path="/BuySellCar-Online/ProcessingDescription" component={ProcessingDescription} />
+        <Route path="/BuySellCar-Online/ProcessModeling" component={ProcessModeling} />
+        <Route path="/BuySellCar-Online/ContextDiagram" component={ContextDiagram} />
+        <Route path="/BuySellCar-Online/DFDDiagram" component={DFDDiagram} />
+        <Route path="/BuySellCar-Online/ProcessDescription" component={ProcessDescription} />
+        <Route path="/BuySellCar-Online/ProcessingDescription" component={ProcessingDescription} />
 
-                    <Route path="/BuySellCar-Online/DataModel" component={DataModel} />
-                    <Route path="/BuySellCar-Online/ERDiagram" component={ERDiagram} />
-                    <Route path="/BuySellCar-Online/DataDictionary" component={DataDictionary} />
-                    <Route path="/BuySellCar-Online/RelationSchema" component={RelationSchema} />
-                    <Route path="/BuySellCar-Online/Normalized" component={Normalized} />
+        <Route path="/BuySellCar-Online/DataModel" component={DataModel} />
+        <Route path="/BuySellCar-Online/ERDiagram" component={ERDiagram} />
+        <Route path="/BuySellCar-Online/DataDictionary" component={DataDictionary} />
+        <Route path="/BuySellCar-Online/RelationSchema" component={RelationSchema} />
+        <Route path="/BuySellCar-Online/Normalized" component={Normalized} />
 
-                    <Route path="/BuySellCar-Online/OutputDesign" component={OutputDesign} />
+        <Route path="/BuySellCar-Online/OutputDesign" component={OutputDesign} />
 
-                    <Route path="/BuySellCar-Online/InputDesign" component={InputDesign} />
-                </Switch>
+        <Route path="/BuySellCar-Online/InputDesign" component={InputDesign} />
+    </Switch>
 
-        )
+)
 
 
